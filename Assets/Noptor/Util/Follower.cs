2644960c699f93f66c75ae43690794c301f9ab60
Diamante <i@almazdeldiablo.ru>
2014-11-06 22:14:45 +0300

    Follwer and random

---
+++ Assets/Noptor/Util/Follower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Noptor.Util
+{
+    /// <summary>
+    /// Следователь — позволяет закреплять один объект за другим при перемещении;
+    /// в отличие от <see cref="Transform.parent"/>, не вращает объект.
+    /// </summary>
+    public class Follower : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Сгдвиг относительно родителя
+        /// </summary>
+        public Vector3 Offset = Vector3.zero;
+
+        /// <summary>
+        /// Цель, за которой следует объект
+        /// </summary>
+        public Transform Target;
+	
+        /// <summary>
+        /// Следование за целью
+        /// </summary>
+        protected void LateUpdate()
+        {
+            transform.position = Target.position + Offset;
+        }
+    }
+}
---
+++ Assets/Noptor/Util/Random.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System;
+
+namespace Noptor.Util
+{
+    public class Random
+    {
+
+        /// <summary>
+        /// Рандом, заданный процентной вероятностью.
+        /// </summary>
+        /// <returns>Ключ в массиве переданных весов, на который выпал рандомайзер.</returns>
+        /// <param name="weights">Массив весов в процентах.</param>
+        public static int PercentRandom(int[] weights)
+        {
+            int[] weightSorted = weights.OrderBy(t => t).ToArray();
+
+            int over = weightSorted.Sum();
+            int rand = UnityEngine.Random.Range(0, over);
+            
+            int top = 0;
+            
+            for (int i = 0; i < weightSorted.Length; i++) {
+                top += weightSorted[i]; 
+                
+                if(rand < top)
+                {
+                    return  Array.FindIndex(weights, w => w == weightSorted[i]);
+//                    int[] matches = Array.FindAll(weights, t => t == weights[i]);
+                }
+            }
+
+            return weights.Length - 1;
+        }
+
+    }
+}
